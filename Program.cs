9f3c35f1af5bbc18dfaff3fb1d56caffd44d7256
erdogansonmez <erdogansonmez@outlook.com>
2024-08-17 23:37:58 +0300

    deployment test

--- Program.cs
+++ Program.cs
@@ -47,11 +47,11 @@ builder.Services.AddDbContext<TransportationContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
