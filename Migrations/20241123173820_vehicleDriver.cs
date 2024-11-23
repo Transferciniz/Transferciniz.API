e9d8637cb6b5f1bb63856d8fafd33f80ad769550
erdogansonmez <erdogansonmez@outlook.com>
2024-11-23 20:40:03 +0300

    migrations

---
+++ Migrations/20241123173820_vehicleDriver.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Transferciniz.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class vehicleDriver : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "DriverId",
+                table: "AccountVehicles",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DriverId",
+                table: "AccountVehicles");
+        }
+    }
+}
